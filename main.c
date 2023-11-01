2729c44b8be8b047e8505434e9c4d6261541cedb
Nermirc <141329241+Nermirc@users.noreply.github.com>
2023-11-01 13:39:51 +0300

    The Student Height problem
    finding the most closer height compared to another ones according to value which is given to us

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define tam 150
+
+int main()
+{
+    int no,boy,fark,minboy,minno;
+    printf("Ogrenci no: \n");
+    scanf("%d",&no);
+    printf("Ogrenci boy : \n");
+    scanf("%d",&boy);
+
+    minno=no;
+    fark=abs(boy-tam);
+    while(no>0){
+        printf("\nOgrenci no:");
+        scanf("%d",&no);
+        printf("Ogrenci boy :");
+        scanf("%d",&boy);
+        if(abs(boy-tam)<fark){
+            fark=abs(boy-tam);
+            minno=no;
+            minboy=boy;
+        }
+    }
+    printf("\n %d numarali ogrenci %d cm boyu ile %d cm'e en yakýndýr",minno,minboy,tam);
+}
